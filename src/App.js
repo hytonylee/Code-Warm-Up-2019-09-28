101963b5d41ce2823a981ee222c346bb79522dea
Tony Lee <hy.tony.lee@gmail.com>
2019-09-28 10:59:10 -0700

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      { id: 1, title: 'my 1st todo', completed: false },
+      { id: 2, title: 'my 2nd todo', completed: false },
+      { id: 3, title: 'my 3rd todo', completed: false },
+    ]
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello World</h1>
+        {
+          this.state.todos.map(todo => {
+            return <div key={todo.id}>{todo.title}</div>
+          })
+        }
+      </div >
+    )
+  }
 }
 
-export default App;
+export default App
