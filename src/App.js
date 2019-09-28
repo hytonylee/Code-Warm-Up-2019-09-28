56aab478569cc1d072b8c3f0108fe980b21e27e8
Tony Lee <hy.tony.lee@gmail.com>
2019-09-28 11:10:33 -0700

    Add Object Mapping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
+    text: '',
     todos: [
       { id: 1, title: 'my 1st todo', completed: false },
       { id: 2, title: 'my 2nd todo', completed: false },
@@ -9,10 +10,28 @@ class App extends React.Component {
     ]
   }
 
+  handleChangeText = e => {
+    const { value } = e.target    //grab value from e.target
+    this.setState({
+      text: value
+    })
+  }
+
+  addToDo = () => {
+    const { todos, text } = this.state
+    this.setState({
+      todos: [...todos, { id: todos.length + 1, title: text, completed: false }],
+      text: ''
+    })
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>Hello World</h1>
+        {/* <h3>{this.state.text}</h3> */}
+        <input value={this.state.text} onChange={this.handleChangeText} />
+        <button onClick={this.addToDo}>Add Text</button>
         {
           this.state.todos.map(todo => {
             return <div key={todo.id}>{todo.title}</div>
